eb945f102cc28f5fff1d319b13502a8fa3bd7768
SleepyLux <152927191+Sleepy-Lux@users.noreply.github.com>
2024-09-20 14:21:13 +0100

    Bug Fixes & Hide secret
    Made Commands into ClientCommands so they can be run in multiplayer
    
    Changed order of Mixin to allow mods to display their information first
    
    Added MathSecret to .gitignore

--- src/main/java/uk/sleepylux/coordspoofer/client/MathSecret.java
+++
@@ -1,7 +0,0 @@
-package uk.sleepylux.coordspoofer.client;
-
-public class MathSecret {
-    public static float MathSecret(float absolute_seed, float multiplier) {
-        return (float) (((absolute_seed * multiplier) / 89.3) * 1.4);
-    }
-}
--- src/main/java/uk/sleepylux/coordspoofer/client/mixin/DebugHudMixin.java
+++ src/main/java/uk/sleepylux/coordspoofer/client/mixin/DebugHudMixin.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import uk.sleepylux.coordspoofer.client.CoordspooferClient;
-import uk.sleepylux.coordspoofer.client.MathSecret;
 
 import java.util.List;
 import java.util.Locale;
